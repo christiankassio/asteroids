5dd4a8583bad55f3d220edc14e41202c4fa42827
Chrisitan Kassio <christiankassio76@gmail.com>
2025-06-12 13:52:20 +1000

    B - Added circleshape and player classes. Player renders

--- main.py
+++ main.py
@@ -3,19 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 def main():
 	pygame.init()
+	fps_clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	print("Starting Asteroids!")
 	print("Screen width: 1280")
 	print("Screen height: 720")
+
 
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-		screen.fill((0,0,0))
+		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
+		dt = (fps_clock.tick(60) / 1000)
+
 
 if __name__ == "__main__":
 	main()
